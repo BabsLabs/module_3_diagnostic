986bea9cb1f8a78d8699f70eabc71ab20c27ff4b
Brian Bower <brian@brianbower.com>
2019-12-09 11:14:11 -0700

    Refactor search index action to use render locals

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,31 +4,6 @@ class SearchController < ApplicationController
     render locals: {
       search_results: PotterHouseSearch.new(params[:house])
     }
-
-
-
-
-    # house = params[:house]
-    # key = ENV['POTTER_API_KEY']
-
-    # I COULD NOT GET THE CONNECTION OBJECT TO WORK AND SPENT WAY TOO LONG TRYING!
-    # conn = Faraday.new(url: "https://www.potterapi.com/") do |faraday|
-    #   faraday.params['key'] = key
-    #   faraday.params['house'] = house
-    # end
-    #
-    # response = conn.get("/v1/characters")
-    #
-    # binding.pry
-
-    # response = Faraday.get "https://www.potterapi.com/v1/characters" do |req|
-    #   req.params['key'] = key
-    #   req.params['house'] = house
-    # end
-    #
-    # members = JSON.parse(response.body, symbolize_names: true)
-    #
-    # @order_members = members.find_all {|member| member[:orderOfThePhoenix]}
   end
 
 end
